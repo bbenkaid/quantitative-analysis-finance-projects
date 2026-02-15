4b5e548ce5c255f738854f395a8cd8031ec8fb6f
bbenkaid <benalia.benkaida19@gmail.com>
2026-02-15 19:38:21 +0100

    Update Analyse quantitative Météo.py

--- Analyse quantitative Météo.py
+++ Analyse quantitative Météo.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import yfinance as yf
 import pandas as pd
@@ -7,34 +6,36 @@ import meteostat as ms
 from datetime import date
 
 tickers = ["^GSPC", "^VIX", "NG=F", "XLU", "CORN"]
-
-# Télécharger les données
-df = yf.download(
+noms = tickers
+def donnees_finance(tickers,noms):
+    D = dict(zip(tickers, noms))
+    # Télécharger les données
+    df = yf.download(
     tickers,
     start="2006-01-01",
     end="2026-01-01",
     interval="1d"
-)
+    )
 
-# Garder UNIQUEMENT les prix de clôture
-df = df.dropna(how="any")
-df = df["Close"]
+    # Garder UNIQUEMENT les prix de clôture
+    df = df.dropna(how="any")
+    df = df["Close"]
 
-# Renommer les colonnes
-df = df.rename(columns={
-    "^GSPC": "SP500",
-    "^VIX": "VIX",
-    "^NG=F": "NG=F",   
-    "XLU": "ELEC",
-    "CORN": "CORN"
-})
+    # Renommer les colonnes
+    df = df.rename(columns=D)
+    # "^GSPC": "SP500",
+    # "^VIX": "VIX",
+    # "^NG=F": "NG=F",   
+    # "XLU": "ELEC",
+    # "CORN": "CORN"
+    # })
+    return df
 
-#Jours d'ouverture des marchés
-openday = [d.strftime("%Y-%m-%d") for d in df.index]
-
+df = donnees_finance(tickers,noms)
 Datafinance = df
+#Returns des actifs 
+returns = Datafinance.pct_change()
 
-#Données météo
 
 START = date(2006, 1, 1)
 END   = date(2026, 1, 1)
@@ -45,18 +46,23 @@ END   = date(2026, 1, 1)
 #Champs de Mais champain Illinois ID KCMI
 station_id = "72530"
 
-ts = ms.daily(station_id, START, END)
-df = ts.fetch()
+def donnee_meteo(START,END,station_id):
+    
+#Données météo
 
-# print(df.head())
-# print(df.columns)
+    ts = ms.daily(station_id, START, END)
+    df = ts.fetch()
+    return df
 
+#Jours d'ouverture des marchés
+openday = [d.strftime("%Y-%m-%d") for d in df.index]
+df = donnee_meteo(START, END, station_id)
 df = df.loc[openday]
 
 #print(df[df['tsun']!= '<NA>'])
 
 
-#Définition des catégories d'événements météo
+#Définition des catégories d'évenements météo
 snow = df[df['snwd']>0]
 rain = df[df['prcp']>20]
 wind = df[df['wspd']>30]
@@ -66,10 +72,10 @@ hum = df[df['rhum']>75] #Humidité dans l'air
 L = [snow,rain,wind,cloud,tb,hum]
 N = ['snow','rain','wind','cloud','tb','hum','norm']
 
-#Création d'un événement jour normal
+#Création d'un évenement jour normal
 Date = []
 for dates in df.index:
-    #df à toute les dates, on veut créer jour normal comme étant un jour sans événements
+    #df à toute les dates, on veut créer jour normal comme étant un jour sans évenements
     S = 0
     for l in L:
         if dates not in l.index:
@@ -81,15 +87,11 @@ Date = [d.strftime("%Y-%m-%d") for d in Date] #format date
 norm = df.loc[Date]
 L.append(norm)
 
-#Liste des dates pour chaque événement
+#Liste des dates pour chaques évenements
 DA = []
 for l in L:
      DA.append([d.strftime("%Y-%m-%d") for d in l.index])
  
-
-#Returns des actifs 
-returns = Datafinance.pct_change()
-
 
 #Rendements moyens dans une liste de dataframes
 K = []
@@ -108,31 +110,41 @@ T = T.rename(columns={
 })
 
 
+
 from pylab import plt,mpl
 
-T.loc["CORN"].plot(kind="bar")
-plt.ylabel("Rendement")
-plt.xlabel("Variables météo")
-plt.title("Impact météo sur CORN")
-plt.show()
+def affiche_impact_meteo(nom):
+    T.loc[nom].plot(kind="bar")
+    plt.ylabel("Rendement")
+    plt.xlabel("Variables météo")
+    plt.title("Impact météo sur " + nom)
+    plt.show()
+
+affiche_impact_meteo("CORN")
 
 
-#Manifestation d'événements binaires, Neige = 1 pas de Neige = 0
+#Manifestation d'évenements binaires, Neige = 1 pas de Neige = 0
 #Objectif corelation avec les returns des actifs
-Z = np.zeros((len(returns.index),7))
-for d in range(len(returns.index)):
-    for i in range(7):
-        if returns.index[d] in L[i].index:
-            Z[d,i] = 1
-            
-df = pd.DataFrame(Z,returns.index,N)
 
-D = df.join(returns)
+def impact_evenement(returns ,liste_evenement):
+    r = len(returns.index)
+    e = len(liste_evenement)
+    Z = np.zeros((r,e))
+    for d in range(r):
+        for i in range(e):
+            if returns.index[d] in liste_evenement[i].index:
+                Z[d,i] = 1
+    df = pd.DataFrame(Z,returns.index,N)
+    D = df.join(returns)
+    return D
 
+D = impact_evenement(returns, L)
+print(D)
 
 import statsmodels.api as sm
 
-X = D[["rain", "snow", "wind", "cloud", "hum", "NG=F", "ELEC"]]
+
+X = D[["rain", "snow", "wind", "cloud", "hum", "NG=F", "XLU"]]
 # X = D[["snow"]]
 y = D["CORN"]
 
@@ -141,55 +153,55 @@ X = sm.add_constant(X)
 model = sm.OLS(y, X, missing="drop").fit()
 print(model.summary())
 
+
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+#Modèle de prévision d'un actif en fonction de la météo
+def prevision_up(nom,returns, liste_evenement):
+    
 
-#Maïs
-N = ['snow','rain','wind','cloud','tb','hum','norm',
-    'up_SP500','up_VIX','up_NG','up_XLU','up_CORN']
+    Df = impact_evenement(returns, liste_evenement)
+    N = list(Df.columns)
+    r = len(returns.index)
+    l = len(Df.columns)
+    e = len(liste_evenement)
+    #Modèle binaire d'évenement météo et de retour up = 1 down = 0, pour prevoir le futur
+    Z = np.zeros((r,l))
+    returns = returns.shift(-1)
+    for d in range(r):
+        for i in range(e):
+            if returns.index[d] in liste_evenement[i].index:
+                Z[d,i] = 1
+        for j in range(e,l):
+            if returns[N[j]].loc[returns.index[d]]>0:
+                Z[d,j] = 1
+    df = pd.DataFrame(Z,returns.index,N)
+    data = pd.concat([df[[c for c in N if c != nom]],df[nom]],axis = 1).dropna()
 
-#Modèle binaire d'événement météo et de retour up = 1 down = 0, pour prevoir le futur
-Z = np.zeros((len(returns.index),12))
-returns = returns.shift(-1)
+    X = data.drop(columns = [nom])
+    y = data[nom]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
-for d in range(len(returns.index)):
-    for i in range(7):
-        if returns.index[d] in L[i].index:
-            Z[d,i] = 1
-    for j in range(7,12):
-        if returns["CORN"].loc[returns.index[d]]>0:
-            Z[d,j] = 1
-df = pd.DataFrame(Z,returns.index,N)
-data = pd.concat([df[N[:1]],df["up_CORN"]],axis = 1).dropna()
-
-
-X = data[N[:1]]
-y = data["up_CORN"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
-
-rf = RandomForestClassifier(
+    rf = RandomForestClassifier(
     n_estimators=600,
     max_depth=None,
     min_samples_leaf=20,
     class_weight="balanced",
     random_state=42
-)
+    )
 
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
 
-rf.fit(X_train, y_train)
-y_pred = rf.predict(X_test)
-
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("\nClassification report:")
-print(classification_report(y_test, y_pred))
-print("\nConfusion matrix:")
-print(confusion_matrix(y_test, y_pred))
-
-
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("\nClassification report:")
+    print(classification_report(y_test, y_pred))
+    print("\nConfusion matrix:")
+    print(confusion_matrix(y_test, y_pred))
 
 
 
-
+prevision_up("CORN", returns, L)
 
